a4096a2ca4ee3b3a73011333d4da46c618f2c584
AluBhorta <dev.bhorta@gmail.com>
2019-11-27 17:09:07 +0600

    set threshold variables for randomized input generation & add generate_random_schedule() function

---
+++ data/rand_data.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+np.random.seed(69)
+
+# vairables to alter for randomization
+
+NUM_OF_ROOMS = 10
+NUM_OF_TIMELSOTS = 20
+NUM_OF_COURSES = 10
+NUM_OF_INSTRUCTORS = 5
+
+MAX_NUM_OF_LECS_PER_COURSE = 5
+MAX_NUM_OF_PREFFERED_ROOMS = NUM_OF_ROOMS/2
+MAX_NUM_OF_QUALIFIED_COURSES_PER_INSTRUCTOR = NUM_OF_COURSES/2
+
+# (DO NOT MODIFY) inferred variables
+
+MAX_LECS_PER_TIMESLOT = NUM_OF_ROOMS if NUM_OF_ROOMS < NUM_OF_INSTRUCTORS else NUM_OF_INSTRUCTORS
+MAX_LECS_THAT_CAN_BE_OFFERED = MAX_LECS_PER_TIMESLOT * NUM_OF_TIMELSOTS
+
+# inputs to the Engine
+
+ROOMS = np.array(["Room-%d"%i for i in np.arange(NUM_OF_ROOMS)])
+
+TIMESLOTS = np.array(["Timeslot-%d"%i for i in np.arange(NUM_OF_TIMELSOTS)])
+
+"""
+(str course_id, int num_of_lectures, int[] preferred_rooms)[]
+"""
+COURSES = np.array([
+	(
+		"CSE%d"%i,
+		np.random.randint(low=1, high=MAX_NUM_OF_LECS_PER_COURSE), 
+		np.random.randint(low=0, high=NUM_OF_ROOMS+1, size=np.random.randint(MAX_NUM_OF_PREFFERED_ROOMS))
+	) for i in np.arange(NUM_OF_COURSES)
+])
+
+"""
+(str instuctor_id, int[] qualified_courses, int[] available_timeslots)[]
+"""
+INSTRUCTORS = np.array([
+    (
+        "INSTRUCTOR%d"%i,
+        list(set(
+            np.random.choice(
+                np.arange(NUM_OF_COURSES),
+                size=np.random.randint(
+                    low=1, 
+                    high=MAX_NUM_OF_QUALIFIED_COURSES_PER_INSTRUCTOR
+                )
+            )
+        )),
+        list(set(
+            np.random.choice(
+                np.arange(NUM_OF_TIMELSOTS),
+                size=np.random.randint(
+                    low=1, 
+                    high=NUM_OF_TIMELSOTS
+                )
+            )
+        )),
+    ) for i in np.arange(NUM_OF_INSTRUCTORS)
+])
+
+
+# sum of lectures of all courses
+LECS_BEING_OFFERED = COURSES[:, 1].sum()
+
+if LECS_BEING_OFFERED > MAX_LECS_THAT_CAN_BE_OFFERED: 
+    raise Exception(
+        "ERROR! Max number of Lectures exceeded.\n" + 
+        "LECS_BEING_OFFERED: %d"%LECS_BEING_OFFERED +  
+        ",\tMAX_LECS_THAT_CAN_BE_OFFERED: %d"%MAX_LECS_THAT_CAN_BE_OFFERED
+    )
+
+
+def generate_random_schedule():
+    # (Room, Timeslot, Course, Instructor)
+    return np.array([
+        (
+            np.random.randint(NUM_OF_ROOMS),
+            np.random.randint(NUM_OF_TIMELSOTS),
+            np.random.randint(NUM_OF_COURSES),
+            np.random.randint(NUM_OF_INSTRUCTORS)
+        ) for i in np.arange(LECS_BEING_OFFERED)
+    ])
+
+    
--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 import numpy as np
 from models.models import Lecture, Course, Instructor, Room, Timeslot
-from data import COURSES, INSTRUCTORS, ROOMS, TIMESLOTS
+from data.rand_data import COURSES, INSTRUCTORS, ROOMS, TIMESLOTS, generate_random_schedule
 
 
 # L = (R[i], T[j], C[k], I[p])
@@ -19,12 +19,18 @@ def encode(lecture: Lecture) -> (int, int, int, int):
     return lecture.encoded_lecture
 
 
-if __name__ == "__main__":
+def print_input():
+    print("\nROOMS: (str room_id) \n",ROOMS)
+    print("\nTIMESLOTS: (str timeslot_id) \n",TIMESLOTS)
+    print("\nCOURSES: (str course_id, int num_of_lectures, int[] preferred_rooms)\n",COURSES)
+    print("\nINSTRUCTORS: (str instuctor_id, int[] qualified_courses, int[] available_timeslots)\n",INSTRUCTORS)
+
+
+def main():
     print("(Room, Timeslot, Course, Instructor)")
     
     # generate a lecture
     l1 = (3,4,1,0)
-    
     print(l1)
 
     L1 = decode(l1)
@@ -33,3 +39,6 @@ if __name__ == "__main__":
     l2 = encode(L1)
     print(l2)
 
+if __name__ == "__main__":
+    print_input()
+    
