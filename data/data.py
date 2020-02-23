3a1578f1faa396b2cff43820a88def0c92e31c48
AluBhorta <dev.bhorta@gmail.com>
2020-02-23 20:14:53 +0600

    complete grs_v2

--- data/data.py
+++ data/data.py
@@ -13,10 +13,10 @@ to import:
 """
 
 """For random generated data"""
-# from data.rand_data import COURSES, INSTRUCTORS, ROOMS, TIMESLOTS, NUM_OF_LECS_BEING_OFFERED, NUM_OF_ROOMS, NUM_OF_TIMELSOTS, NUM_OF_COURSES, NUM_OF_INSTRUCTORS
+from data.rand_data import COURSES, INSTRUCTORS, ROOMS, TIMESLOTS, NUM_OF_LECS_BEING_OFFERED, NUM_OF_ROOMS, NUM_OF_TIMELSOTS, NUM_OF_COURSES, NUM_OF_INSTRUCTORS
 
 
 """For data parsed from CSV"""
-from data.input_as_csv.parse_csv import parse_csv_from
-ROOMS, TIMESLOTS, COURSES, INSTRUCTORS, NUM_OF_ROOMS, NUM_OF_TIMELSOTS, NUM_OF_COURSES, NUM_OF_INSTRUCTORS, NUM_OF_LECS_BEING_OFFERED, MAX_LECS_PER_TIMESLOT = parse_csv_from(
-    "data/input_as_csv/" + "iub/")
+# from data.input_as_csv.parse_csv import parse_csv_from
+# ROOMS, TIMESLOTS, COURSES, INSTRUCTORS, NUM_OF_ROOMS, NUM_OF_TIMELSOTS, NUM_OF_COURSES, NUM_OF_INSTRUCTORS, NUM_OF_LECS_BEING_OFFERED, MAX_LECS_PER_TIMESLOT = parse_csv_from(
+#     "data/input_as_csv/" + "iub/")
