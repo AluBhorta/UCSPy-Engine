b8fc96a09bf8cfc98ef52c62edb8a5344b289386
alu@ubuntu20-ZeDHP <dev.bhorta@gmail.com>
2020-08-05 11:03:08 +0600

    add default_expo fitness func

--- core/fitness/__init__.py
+++ core/fitness/__init__.py
@@ -1,7 +1,9 @@
 from core.fitness.default import default_fitness
+from core.fitness.def_expo import default_expo_fitness
 
 
 FITNESS_FUNCS = {
-    "default": default_fitness
+    "default": default_fitness,
+    "default_expo": default_expo_fitness,
 }
 
