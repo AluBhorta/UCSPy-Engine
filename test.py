405ce8bf2691ae005da92120f5428cc3e9aa042c
alu@ubuntu20-ZeDHP <dev.bhorta@gmail.com>
2020-08-31 17:01:07 +0600

    add solver test update travis config

---
+++ test.py
@@ -0,0 +1,5 @@
+
+from tests import UCSPyEngineTest
+
+if __name__ == "__main__":
+    UCSPyEngineTest().run_tests()
---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+
+from core.models.UCSPyEngine import UCSPyEngine
+
+
+class UCSPyEngineTest:
+    def __init__(self, config_file="ucsp.config.json"):
+        self.ucspy_engine = UCSPyEngine()
+
+    def run_tests(self):
+        self._test_solve("ga", epochs=5)
+        self._test_solve("meme", epochs=5)
+        
+    def _test_solve(self, *a, **kw):
+        s = self.ucspy_engine.solve(*a, **kw)
+        assert hasattr(s, "classes"), "Invalid return type from UCSPyEngine.solve()!"
